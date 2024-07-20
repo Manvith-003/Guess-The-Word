ee6446e2fda78e5a18da7d64994fe06425367840
Manvith <manvithpoojary63@gmail.com>
2024-07-20 10:23:07 +0530

    gameing website

---
+++ game.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="main">
+        <div class="box">
+            <div class="container">
+                <h3 id="answer"></h3>
+                <h3 id="question"></h3>
+                <input type="text" id="text">
+            </div>
+            <div class="containerbtn">
+                <button id="btnshow">SHOW</button>
+                <button id="btncheck">CHECK</button>
+                <button id="btnnext">NEXT</button>
+            </div>
+        </div>
+    </div>
+ 
+</body>
+</html>
